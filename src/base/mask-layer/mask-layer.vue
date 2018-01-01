ced119d887bb838c3984d9a5be39de6b88eed594
jianzhongmin <282126990@qq.com>
2018-01-01 23:47:13 +0800

    1.0.6

---
+++ src/base/mask-layer/mask-layer.vue
@@ -0,0 +1,8 @@
+<template>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/common/js/util.js
@@ -0,0 +1,29 @@
+// 计算出随机数
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+export function shuffle (arr) {
+  let _arr = arr.slice()
+  for (let i = 0; i < _arr.length; i++) {
+    let j = getRandomInt(0, i)
+    let t = _arr[i]
+    _arr[i] = _arr[j]
+    _arr[j] = t
+  }
+  return _arr
+}
+
+// 节流函数
+export function debounce (func, delay) {
+  let timer
+
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      func.apply(this, args)
+    }, delay)
+  }
+}
